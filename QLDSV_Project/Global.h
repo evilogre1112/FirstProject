03e5637294704eed7a296914fab9572fa90c947b
NhatPhi <phinhatlam123@gmail.com>
2026-02-04 11:00:04 +0700

    them chuong trinh con main va Global quy dinh

---
+++ QLDSV_Project/Global.h
@@ -0,0 +1,19 @@
+
+/*
+    Global chứa các hàm quy định từ trước, tất cả
+    các hàm sau này đều phải được khai báo trong
+    Global và dùng đúng cấu trúc.
+*/
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+
+#endif
---
+++ QLDSV_Project/main.cpp
@@ -0,0 +1,9 @@
+//Hạn chế sử dụng bits/stdc++.h, tốn bộ nhớ
+#include <iostream>
+#include <Global.h>
+
+using namespace std ;
+
+int main(){
+    return 0;
+}
