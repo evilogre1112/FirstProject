341585733760c8a7a6883d2feacf0ce6df195093
Viet map <viethoangthtv111@gmail.com>
2026-03-03 20:21:25 +0700

    theem file format, du lieu test, ham nhap tu file

--- QLDSCB_Project/Global.cpp
+++ QLDSCB_Project/Global.cpp
@@ -22,36 +22,36 @@ DateTime::DateTime (){
         yy=2026;
     }
 
-short DateTime::get_hh(){
+int DateTime::get_hh(){
         return hh;
 }
-short DateTime::get_mm(){
+int DateTime::get_mm(){
     return mm;
 }
-short DateTime::get_dd(){
+int DateTime::get_dd(){
         return dd;
 }
-short DateTime::get_mt(){
+int DateTime::get_mt(){
         return mt;
 }
 int DateTime::get_yy(){
         return yy;
 }
-bool DateTime::set_hh(short h){
+bool DateTime::set_hh(int h){
     if (h >= 0 && h <= 23) {
         hh = h;
         return true;
     }
     return false;
 }
-bool DateTime::set_mm(short m){
+bool DateTime::set_mm(int m){
     if (m >= 0 && m <= 59) {
         mm = m;
         return true;
     }
     return false;
 }
-bool DateTime::set_mt(short t){
+bool DateTime::set_mt(int t){
     if (t >= 1 && t <= 12) {
         mt = t;
         return true;
@@ -65,9 +65,9 @@ bool DateTime::set_yy(int y){
     }
     return false;
 }
-bool DateTime::set_dd(short d){
+bool DateTime::set_dd(int d){
     short thang[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (yy % 4==0 && yy % 100 == 0) thang[2]=29;
+    if (yy % 4==0 && yy % 100 != 0|| yy%400==0) thang[2]=29;
     if (d >= 1 && d <= thang[mt]) {
         dd = d;
         return true;
@@ -108,6 +108,64 @@ CB::CB(){
             delete[] DSV;
         }
     }
+    bool CB::set_maCB(char ma[maCB_max])
+    {
+        if(ma==NULL || strlen(ma)==0) return false; //kiểm tra rỗng
+        strncpy(this->maCB, ma, maCB_max);
+        this->maCB[maCB_max-1] ='\0';
+        //bảo đảm chuỗi luôn có điểm kết thuc
+        return true;
+    }
+    bool CB::set_ngayHK(int h, int m, int y, int mmt, int d){
+        bool check = true;  
+        check &= this->ngayKH.set_yy(y);
+        check &= this->ngayKH.set_mt(mmt);
+        check &= this->ngayKH.set_dd(d);
+        check &= this->ngayKH.set_hh(h);
+        check &= this->ngayKH.set_mm(m);
+    return check; // Trả về true nếu tất cả các giá trị đều hợp lệ
+
+    }
+    bool CB::set_sbDich(char *sbd){
+        int n=strlen(sbd);
+        if(sbd==NULL|| n==0) return false;
+
+        // giải phóng bộ nhớ nếu còn
+        if( this->sbDich != NULL ) delete[] this->sbDich;
+        this->sbDich = new char[n+1];
+        strncpy(this->sbDich,sbd,n);
+        this->sbDich[n] = '\0';
+        return true;
+    }
+    bool CB::set_trangThai(int i){
+        // Kiểm tra i có nằm trong phạm vi 0-3 không
+        if (i >= 0 && i <= 3) {
+            this->trangThai = i;
+            return true;
+        }
+        return false;
+    }
+    bool CB::set_soHieuMB(char *shmb){
+        int n=strlen(shmb);
+        if(shmb==NULL|| n==0) return false;
+
+        strncpy(this->soHieuMB,shmb,n);
+        this->soHieuMB[n] = '\0';
+        return true;
+    }
+    bool CB::set_socho(int c){
+        if(c<=0) return false;
+
+        this->socho = c;
+        // có số chỗ rồi ta tạo mảng danh sách vé<cmnd>
+        this->DSV =new char*[c];
+        for(int i=0;i<c;i++){
+            this->DSV[i] = new char[cmnd_max];
+            strcpy(this->DSV[i],"0");
+        }
+        return true;
+        
+    }
     listCB::listCB(){
         slCB = 0;
         head = NULL;
@@ -115,34 +173,55 @@ CB::CB(){
 
     // ---- cấu trúc hành khách ----//
     HK::HK(){
-        ho = new char[41];
-        ten = new char[16];
-        cmnd = new char[14];
+        ho =NULL;
+        ten =NULL;
+        cmnd = NULL;
         phai = false;
         left = right = NULL;
     }
 
     
-    bool HK::set_cmnd(){
-        cin.getline(cmnd,14);
-        int n = strlen(cmnd);
+    bool HK::set_cmnd(char *new_cmnd){
+        if(new_cmnd ==NULL) return false;
+        int n = strlen(new_cmnd);
         for(int i = 0; i < n; i++)
-        if(cmnd[i] < '0' || cmnd[i] > '9') return false;
+            if(new_cmnd[i] < '0' || new_cmnd[i] > '9') return false;
+        //giải phóng bộ nhớ nếu có
+        if(this->cmnd!= NULL)  delete[] this->cmnd;
+        // cấp lại bộ nhớ mới'
+        this->cmnd= new char[n+1];
+        //dùng strncpy để sao chép an toàn
+        strncpy(this->cmnd, new_cmnd,n);
+        this->cmnd[n]='\0';
         return true;           
     }
 
     bool HK::set_ho(char *new_ho){
+        if(new_ho ==NULL) return false;
         int n = strlen(new_ho);
         for(int i = 0; i < n; i++)
-        if(new_ho[i] >= '0' && new_ho[i] <= '9') return false;
-        ho = new_ho;
+            if(new_ho[i] >= '0' || new_ho[i] <= '9') return false;
+        //giải phóng bộ nhớ nếu có
+        if(this->ho!= NULL)  delete[] this->ho;
+        // cấp lại bộ nhớ mới'
+        this->ho= new char[n+1];
+        //dùng strncpy để sao chép an toàn
+        strncpy(this->ho, new_ho,n);
+        this->ho[n]='\0';
         return true;
     }
     bool HK::set_ten(char *new_ten){
+       if(new_ten ==NULL) return false;
         int n = strlen(new_ten);
         for(int i = 0; i < n; i++)
-        if(new_ten[i] >= '0' && new_ten[i] <= '9') return false;
-        ten = new_ten;
+            if(new_ten[i] >= '0' || new_ten[i] <= '9') return false;
+        //giải phóng bộ nhớ nếu có
+        if(this->ten!= NULL)  delete[] this->ten;
+        // cấp lại bộ nhớ mới'
+        this->ten= new char[n+1];
+        //dùng strncpy để sao chép an toàn
+        strncpy(this->ten, new_ten,n);
+        this->ten[n]='\0';
         return true;
     }
 
@@ -182,8 +261,8 @@ CB::CB(){
     // ---- đọc file ----
 
 
-    bool set_CB(listCB &dsCB, const char *cb){
-        ifstream f(cb);                         // mở file cb
+    bool set_CB(listCB &dsCB, const char *path_file_CB){
+        ifstream f(path_file_CB);                         // mở file cb
         if( !f.is_open() ) return false;
         dsCB.slCB=0;
         dsCB.head = NULL;
@@ -191,25 +270,25 @@ CB::CB(){
         
         while(!f.eof()){
             CB* tmp =new CB();
-            f.getline(tmp->maCB,maCB_max,' ');
+            f.getline(tmp->maCB,maCB_max,'|');
             //---doc ngay thang---
             f >> tmp->ngayKH.hh;
             f.ignore();
             f >> tmp->ngayKH.mm;
             f.ignore();
-            f >> tmp->ngayKH.dd;
+            f >> tmp->ngayKH.yy;
             f.ignore();
             f >> tmp->ngayKH.mt;
             f.ignore();
-            f >> tmp->ngayKH.yy;
+            f >> tmp->ngayKH.dd;
             f.ignore();
             //---sân bay đích---
             tmp->sbDich = new char[50];
-            f.getline(tmp->sbDich,50 ,' ');
+            f.getline(tmp->sbDich,50 ,'|');
             //---trang thai---
             f>>tmp->trangThai;
             //---số hiệu máy bay---
-            f.getline(tmp->soHieuMB, soHieuMB_max,' ');
+            f.getline(tmp->soHieuMB, soHieuMB_max,'|');
             //---số chỗ ----
             f >> tmp->socho;
             //---mảng danh sách cmnd----
@@ -236,20 +315,20 @@ CB::CB(){
         return true;
     }
 
-    bool set_MB(listMB &dsMB, const char *mb){
-        ifstream f(mb);
+    bool set_MB(listMB &dsMB, const char *path_file_MB){
+        ifstream f(path_file_MB);
         if(!f.is_open()) return false;
         dsMB.slMB=0;
         while(dsMB.slMB < slMB_max&& !f.eof() ){//đọc đến cuối file
             MB* tmp =new MB();                  //tạo ô nhớ chưaas MB
-            f.getline(tmp->soHieuMB,soHieuMB_max,' ');
+            f.getline(tmp->soHieuMB,soHieuMB_max,'|');
             // --- số hiệu máy bay----
             if(strlen(tmp->soHieuMB)== 0||f.eof()){
                 delete tmp;
                 break;
             }
             //---loai mb----
-            f.getline(tmp->loaiMB, loaiMB_max,' ');
+            f.getline(tmp->loaiMB, loaiMB_max,'|');
             f>> tmp->socho;
             // xóa dáu cách còn lại dấu chống dòng
             f.ignore();
@@ -259,26 +338,69 @@ CB::CB(){
         f.close();
         return true;
     }
-
+    // chèn  vào cây nhị phân so sánh cmnd
+    /*HK*: Khai báo rằng goc là một con trỏ, nó lưu trữ địa chỉ của một đối tượng HK.
+        &: Khai báo rằng goc được truyền vào theo dạng tham chiếu. 
+    Điều này có nghĩa là mọi thay đổi đối với goc bên trong hàm sẽ
+        thay đổi trực tiếp biến gốc ở ngoài hàm.
+    */
     bool set_KH_tree(HK* & goc, HK * moi){
-
+        if(goc == NULL){
+            goc = moi;
+            return true;
+        }
+        // so sánh cmnd
+        int res= strcmp(moi->cmnd, goc->cmnd);
+        if( res<0){
+            return set_KH_tree(goc->left,moi);
+        } else if ( res>0){
+            return set_KH_tree(goc->right,moi);
+        }
+        //nhập bị trùng
+        return false;
     }
-    bool set_HK(listHK &dsHK, const char *hk){
-        ifstream f(hk);
+    bool set_HK(listHK &dsHK, const char *path_file_HK){
+        ifstream f(path_file_HK);
         if(!f.is_open()) return false;
         dsHK.goc =NULL;         // cây rỗng
         dsHK.slHK=0;
+        // biến rỗng chứa dũ liệu đầu vào để tránh tạo new thừa
+        char b_cmnd[cmnd_max], b_ten[ten_max], b_ho[ho_max];
+        bool b_phai;
         while(!f.eof()){
             HK* tmp = new HK();
-            
+            f.getline(b_cmnd,cmnd_max,'|');
+            // kt hết file chưa 
+            if(f.eof()||strlen(b_cmnd)==0) break;
+            //  nhập dũ liệu vào
+            f.getline(b_ho,ho_max,'|');
+            f.getline(b_ten,ten_max,'|');
+            f>>b_phai;
+
+            //cập nhật dữ liệu
+            bool ok=true;   
+            // lưu trạng thái nhập xem có bị sai định dạng không
+            ok &= tmp->set_cmnd(b_cmnd);
+            ok &= tmp->set_ho(b_ho);
+            ok &= tmp->set_ten(b_ten);
+            tmp->phai = b_phai;
+            tmp->left = tmp->right = NULL;
+            // nếu dữ liệu hợp lệ  thì chèn vào cây 
+            if (ok && set_KH_tree(dsHK.goc,tmp)){
+                dsHK.slHK++;
+            }else {
+                // nếu sai dữ liêu  thì xóa nó
+                delete tmp;
+            }
         }
+        f.close();
         return true;
     }
 
-    bool set_file(listMB &dsMB, listCB &dsCB, listHK &dsHK, const char *mb, const char *cb, const char *hk ){
-        bool ok1 = set_MB(dsMB , mb);
-        bool ok2= set_CB(dsCB , cb);
-        bool ok3 = set_HK(dsHK , hk);
 
-        return ok1&&ok2&&ok3;
-    }
+
+
+
+
+
+
--- QLDSCB_Project/Global.h
+++ QLDSCB_Project/Global.h
@@ -23,6 +23,11 @@ const int soHieuMB_max = 15;
 const int loaiMB_max = 40;
 const int maCB_max= 15;
 const int cmnd_max=13;
+const int ho_max=55;
+const int ten_max=14;
+const char* path_file_CB="Data/DSCB.txt";
+const char* path_file_MB="Data/DSMB.txt";
+const char* path_file_HK="Data/DSHK.txt";
 
 // --- cấu trúc máy bay ---
 struct MB {
@@ -45,18 +50,17 @@ struct MB_Stat {
 
 // --- cấu trúc chuyến bay ---
 struct DateTime{
-    short hh, mm, dd, mt;
-    int yy;
+    int hh, mm, dd, mt, yy;
     DateTime();
-    short get_hh();
-    short get_mm();
-    short get_dd();
-    short get_mt();
+    int get_hh();
+    int get_mm();
+    int get_dd();
+    int get_mt();
     int get_yy();
-    bool set_hh(short h);           
-    bool set_mm(short m);
-    bool set_dd(short d);
-    bool set_mt(short t);
+    bool set_hh(int h);           
+    bool set_mm(int m);
+    bool set_dd(int d);
+    bool set_mt(int t);
     bool set_yy(int y);
 };
 
@@ -67,10 +71,16 @@ struct CB{
     int trangThai;                    // 0 hủy chuyến, 1 còn vé, 2 hết vé, 3 hoàn tất
     char soHieuMB[soHieuMB_max];      // số hiệu máy bay
     int socho;                        // số chỗ trên máy bay
-    char **DSV;                       // mảng cáp phats động chứa cmnd chảu khách hàng
+    char **DSV;                       // mảng cấp phát động chứa cmnd chảu khách hàng
     CB* next;                         // chứa địa chỉ contro tiếp theo
     CB();                             // ko tham số
     CB(int sc);
+    bool set_maCB(char ma[maCB_max]);
+    bool set_ngayHK(int h, int m, int y, int mmt, int d);   //nhập vào theo giờ, phút, năm, tháng, ngày.
+    bool set_sbDich(char *sbd);
+    bool set_trangThai(int i);
+    bool set_soHieuMB(char *shmb);
+    bool set_socho(int c);
 
     ~CB();
 };
@@ -83,9 +93,9 @@ struct listCB{
 
 // --- cấu trúc hành khách cây nhị phân ---
 struct HK{
-    char* ho;                           // họ và tên đệm chứa 40 kí tự
-    char* ten;                          // ten cho 15 ki tu
-    char* cmnd;                         // cmnd có 13 kí tự
+    char *ho;                           // họ và tên đệm chứa 40 kí tự
+    char *ten;                          // ten cho 15 ki tu
+    char *cmnd;                         // cmnd có 13 kí tự
     bool phai;                     // true = gái   ;false = trai
     HK *left, *right;                  
     HK();
@@ -93,7 +103,7 @@ struct HK{
     bool set_ho(char *new_ho);          // true thì đã cập nhật tên họ mới 
     bool set_ten(char *new_ten);
     char* get_ho();                   // trả về tên đệm vầ họ
-    bool set_cmnd();                  // nhâp cmnd
+    bool set_cmnd(char *new_cmnd);                  // nhâp cmnd
     char* get_cmnd();                 // trả về cmnd
     char* get_ten();
     bool set_phai(bool gt);
@@ -107,35 +117,27 @@ struct listHK{
 
 };
 
-// hàm nhập dữ liệu từ file
+/**
+ * @brief đọc file chuyến bay
+ * @param dsMB danh sách chuyến bay
+ * @param path_file_MB đường dẫn file chuyến bay
+ * @return true nếu lấy thành công
+ */
 
-/*
-    1.  số lượng chuyến bay
-    2.  mã chuyến bay 
-    3.  ngày khưởi hành
-    4.  sân bay đích
-    5.  trạng thái 
-    6.  số hiệu máy bay
-    7.  số chỗ
-    8.  mảng danh sách cmnd
-    9.  con trỏ next
-*/
-bool set_CB(listCB &dsCB, const char *cb);
-
-/*
-    1. số lượng máy bay
-    2. số hiệu máy bay
-    3. loại máy bay
-    4. số chỗ
-*/
-bool set_MB(listMB &dsMB, const char *mb);
+bool set_CB(listCB &dsCB, const char *path_file_CB);
+/**
+ * @brief đọc file máy bay
+ * @param dsMB danh sách máy bay
+ * @param path_file_MB đường dẫn file máy bay
+ * @return true nếu lấy thành công
+ */
+bool set_MB(listMB &dsMB, const char *path_file_MB);
 /*
     1.  ho 
     2.  ten
     3.   cmnd
 */
-bool set_HK(listCB &dsHK, const char *hk);
+bool set_HK(listHK &dsHK, const char *path_file_HK);
 
-//--- 1 lần đọc 3 file ---
-bool set_file(listMB &dsMB, listCB &dsCB, listHK & dsHK, const char *mb, const char *cb, const char *hk );
+
 #endif
